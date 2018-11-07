f4368ccbd3887aebfa5959599cd1f4506034f41b
axlevisu <axlevisu@gmail.com>
2018-11-07 23:45:15 +0530

    removing debug

--- app.py
+++ app.py
@@ -150,5 +150,4 @@ def analysis():
     # return "henlo"
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run(host='127.0.0.1', port=7000,debug=True)
+    app.run(host='127.0.0.1', port=7000)
